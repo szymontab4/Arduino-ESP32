05572f96049ca6521007c34fa6411c896250a98f
szymontab4 <szymontab4@gmail.com>
2026-03-11 11:36:06 +0100

    Kod testowy
    Brak aktualizacji przez ostatnie dni był spowodowany przez problemy z elektroniką, więcej w readme

--- automatyczne_podlewanie/automatyczne_podlewanie.ino
+++ automatyczne_podlewanie/automatyczne_podlewanie.ino
@@ -1,9 +1,38 @@
+const int pompa=4, zasilanie=2, odczyt=A0;
+
 void setup() {
   // put your setup code here, to run once:
+  pinMode(pompa, OUTPUT);
+  pinMode(zasilanie, OUTPUT);
+  pinMode(odczyt, INPUT);
+  Serial.begin(9600);
 
+  
 }
 
 void loop() {
+  delay(2000);
+  digitalWrite(pompa, HIGH);
+  delay(500);
+  digitalWrite(pompa, LOW);
+  for(int i=0; i<5; i++)
+  {
+    delay(1000);
+    digitalWrite(pompa, HIGH);
+    delay(2000);
+    digitalWrite(pompa, LOW);
+  }
   // put your main code here, to run repeatedly:
-
+  /*digitalWrite(zasilanie, HIGH);
+  delay(500);
+  int wartosc=analogRead(odczyt);
+  Serial.println(wartosc);
+  digitalWrite(zasilanie, LOW);
+  if(wartosc>2000);
+  {
+    digitalWrite(pompa, HIGH);
+    delay(5000);
+    digitalWrite(pompa, LOW);
+  }
+  delay(5000);*/
 }
