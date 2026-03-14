b7e923ec61a637c55a567a9cac1d743febe65f00
szymontab4 <szymontab4@gmail.com>
2026-03-14 13:13:29 +0100

    Update automatyczne_podlewanie.ino
    Czujnik wilgotności już znajduje sie w doniczce, silnik jeszcze nie podłączony bo czekam na przyjęcie się rośliny

--- automatyczne_podlewanie/automatyczne_podlewanie.ino
+++ automatyczne_podlewanie/automatyczne_podlewanie.ino
@@ -6,9 +6,8 @@ const char* ssid = "FunBox2-33BF";
 const char* password = "Falafelwpowietrzu";
 const char* discord_url = "https://discord.com/api/webhooks/1481352509392945233/W6FFZ42COL14CrWsFM5sIO5ZfmKaDS1oQH8RXgjz98ZZjN0IE61rRzJyz3wS7iyBUm3f";
 
-
 const int pompa=4, zasilanie=2, odczyt=A0; //piny
-const int czas_pompy=3 /*w sekundach*/, czas_snu=1*60*60 /*w godzinach*/, wartosc_graniczna_czujnika=2000; /*wartosc z konsoli*/  //zmienne
+const int czas_pompy=3 /*w sekundach*/, czas_snu=/*2*60*60*/30*60 /*w godzinach*/, wilgotnosc_0=2600, wilgotnosc_100=1100; /*wartosc z konsoli*/  //zmienne
 
 #define uS_TO_S_FACTOR 1000000ULL
 
@@ -19,24 +18,29 @@ void setup() {
   pinMode(odczyt, INPUT);
   Serial.begin(9600);
 
-  //POŁĄCZENIE Z WIFI
-  WiFi.begin(ssid, password);
-  unsigned long startAttemptTime = millis();
-  while (WiFi.status() != WL_CONNECTED && millis() - startAttemptTime < 10000) {
-    delay(500);
-    Serial.print(".");
-  }
-
-
+  // --- 1. ODCZYT Z CZUJNIKA (W CAŁKOWITEJ CISZY RADIOWEJ) ---
   digitalWrite(zasilanie, HIGH); //wlaczenie zasilania dla czujnika wilgotnosci
   delay(500);
-  int wartosc_czujnika=analogRead(odczyt);
+  
+  // Seria 10 pomiarów do wyciągnięcia średniej
+  int suma_odczytow = 0;
+  for(int i = 0; i < 10; i++) {
+    suma_odczytow += analogRead(odczyt);
+    delay(50); // krótka przerwa między próbkami
+  }
+  int wartosc_czujnika = suma_odczytow / 10; // Obliczenie średniej
+  
   Serial.println(wartosc_czujnika);
   digitalWrite(zasilanie, LOW);
 
   String status_podlewania="Nie";
 
-  if(wartosc_czujnika>wartosc_graniczna_czujnika)
+  // Użycie funkcji map i zabezpieczenie constrain
+  int odczytana_wilgotnosc = map(wartosc_czujnika, wilgotnosc_0, wilgotnosc_100, 0, 100);
+  odczytana_wilgotnosc = constrain(odczytana_wilgotnosc, 0, 100);
+
+  // --- 2. LOGIKA PODLEWANIA ---
+  if(odczytana_wilgotnosc<45)
   {
     digitalWrite(pompa, HIGH);
     delay(czas_pompy*1000);
@@ -44,24 +48,29 @@ void setup() {
     status_podlewania="Tak";
   }
 
-  //Wysylanie danych na discord
+  // --- 3. POŁĄCZENIE Z WIFI ---
+  WiFi.begin(ssid, password);
+  unsigned long startAttemptTime = millis();
+  while (WiFi.status() != WL_CONNECTED && millis() - startAttemptTime < 10000) {
+    delay(500);
+    Serial.print(".");
+  }
+
+  // --- 4. WYSYŁANIE DANYCH NA DISCORD ---
   if (WiFi.status() == WL_CONNECTED) {
     HTTPClient http;
     http.begin(discord_url);
     http.addHeader("Content-Type", "application/json");
 
-    String msg = "{\"content\": \"Wilgotnosc: " + String(wartosc_czujnika) + ". Podlewanie: " + status_podlewania + "\"}";
+    String msg = "{\"content\": \"Wilgotnosc: " + String(odczytana_wilgotnosc) + "%. Podlewanie: " + status_podlewania + "\"}";
     http.POST(msg);
     http.end();
   }
 
-
-  //usypianie
+  // --- 5. USYPIANIE ---
   Serial.flush();
-  esp_sleep_enable_timer_wakeup(uS_TO_S_FACTOR*10);
+  esp_sleep_enable_timer_wakeup(uS_TO_S_FACTOR*czas_snu);
   esp_deep_sleep_start();
-
-  
 }
 
 void loop() {
